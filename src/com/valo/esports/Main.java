5fc98155494350c25c8285a6d9f7263fa5c8d47c
Flowjaro <lassoluis87@gmail.com>
2026-04-14 19:29:43 -0500

    Se agrego casos y un menu interactivo para demostracion

--- src/com/valo/esports/Main.java
+++ src/com/valo/esports/Main.java
@@ -13,38 +13,93 @@ public class Main {
         UserController userController = new UserController();
         CategoryController categoryController = new CategoryController();
 
-        System.out.println("=== VALORANT ESPORTS SYSTEM ===");
+        int option;
 
-        // REGISTRO
-        System.out.print("Enter username: ");
-        String username = sc.nextLine();
+        do {
+            System.out.println("\n=== VALORANT ESPORTS SYSTEM ===");
+            System.out.println("1. Register User");
+            System.out.println("2. Login");
+            System.out.println("3. Deactivate User");
+            System.out.println("4. Create Category");
+            System.out.println("5. Show Categories");
+            System.out.println("6. Update Category");
+            System.out.println("7. Delete Category");
+            System.out.println("0. Exit");
 
-        System.out.print("Enter password: ");
-        String password = sc.nextLine();
+            System.out.print("Choose option: ");
+            option = sc.nextInt();
+            sc.nextLine(); // limpiar buffer
 
-        System.out.print("Enter email: ");
-        String email = sc.nextLine();
+            switch (option) {
 
-        userController.register(username, password, email);
+                case 1:
+                    System.out.print("Username: ");
+                    String username = sc.nextLine();
 
-        // LOGIN
-        System.out.println("\n--- LOGIN ---");
-        userController.login(username, password);
+                    System.out.print("Password: ");
+                    String password = sc.nextLine();
 
-        // INACTIVAR
-        System.out.println("\n--- DEACTIVATE USER ---");
-        userController.deactivate(username);
+                    System.out.print("Email: ");
+                    String email = sc.nextLine();
 
-        // LOGIN FALLA
-        System.out.println("\n--- LOGIN AGAIN ---");
-        userController.login(username, password);
+                    userController.register(username, password, email);
+                    break;
 
-        // CATEGORY
-        System.out.println("\n--- CATEGORY ---");
-        categoryController.create("FPS");
-        categoryController.create("MOBA");
+                case 2:
+                    System.out.print("Username: ");
+                    String loginUser = sc.nextLine();
 
-        categoryController.getAll();
+                    System.out.print("Password: ");
+                    String loginPass = sc.nextLine();
+
+                    userController.login(loginUser, loginPass);
+                    break;
+
+                case 3:
+                    System.out.print("Username to deactivate: ");
+                    String userToDeactivate = sc.nextLine();
+
+                    userController.deactivate(userToDeactivate);
+                    break;
+
+                case 4:
+                    System.out.print("Category name: ");
+                    String name = sc.nextLine();
+
+                    categoryController.create(name);
+                    break;
+
+                case 5:
+                    categoryController.getAll();
+                    break;
+
+                case 6:
+                    System.out.print("Category ID: ");
+                    int updateId = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("New name: ");
+                    String newName = sc.nextLine();
+
+                    categoryController.update(updateId, newName);
+                    break;
+
+                case 7:
+                    System.out.print("Category ID: ");
+                    int deleteId = sc.nextInt();
+
+                    categoryController.delete(deleteId);
+                    break;
+
+                case 0:
+                    System.out.println("Exiting...");
+                    break;
+
+                default:
+                    System.out.println("Invalid option");
+            }
+
+        } while (option != 0);
 
         sc.close();
     }
