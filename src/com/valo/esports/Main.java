50cf61db2832d50e491ad1a574f5191319c4a659
Flowjaro <lassoluis87@gmail.com>
2026-04-14 19:25:35 -0500

    Se agrego Scanner

--- src/com/valo/esports/Main.java
+++ src/com/valo/esports/Main.java
@@ -3,28 +3,49 @@ package com.valo.esports;
 import com.valo.esports.controller.UserController;
 import com.valo.esports.controller.CategoryController;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        UserController controller = new UserController();
-        
-        // prueba registro
-        controller.register("luis", "1234", "luis@mail.com");
-        
-        // prueba login
-        controller.login("luis", "1234");
-        
-        // prueba desactivar            
-        controller.deactivate("luis");
-        
-        CategoryController categoryController = new CategoryController();   
+        Scanner sc = new Scanner(System.in);
+
+        UserController userController = new UserController();
+        CategoryController categoryController = new CategoryController();
+
+        System.out.println("=== VALORANT ESPORTS SYSTEM ===");
+
+        // REGISTRO
+        System.out.print("Enter username: ");
+        String username = sc.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = sc.nextLine();
+
+        System.out.print("Enter email: ");
+        String email = sc.nextLine();
+
+        userController.register(username, password, email);
+
+        // LOGIN
+        System.out.println("\n--- LOGIN ---");
+        userController.login(username, password);
+
+        // INACTIVAR
+        System.out.println("\n--- DEACTIVATE USER ---");
+        userController.deactivate(username);
+
+        // LOGIN FALLA
+        System.out.println("\n--- LOGIN AGAIN ---");
+        userController.login(username, password);
+
+        // CATEGORY
+        System.out.println("\n--- CATEGORY ---");
         categoryController.create("FPS");
         categoryController.create("MOBA");
+
         categoryController.getAll();
-        categoryController.update(1, "Shooter");
-        categoryController.delete(2);
-        categoryController.getAll();
- 
- 
+
+        sc.close();
     }
 }
